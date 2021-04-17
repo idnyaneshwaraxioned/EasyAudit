a9662d19d280e31bc8b1d03247a32013b27715bb
idnyaneshwaraxioned <dnyaneshwari@prdxn.com>
2021-04-17 10:13:44 +0530

    slider js done

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,46 @@
 
 */
 
+$(document).ready(function () {
+
+	$('.banner-slider').slick({
+	});
+
+	$('.team-slider').slick({
+		centerMode: true,
+		centerPadding: '60px',
+		slidesToShow: 3,
+		responsive: [
+			{
+				breakpoint: 997,
+				settings: {
+					arrows: true,
+					centerMode: true,
+					centerPadding: '40px',
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					arrows: true,
+					centerMode: true,
+					centerPadding: '40px',
+					slidesToShow: 1
+				}
+			}
+		]
+	});
+
+
+	// ============== humburger btn===============
+
+	$('.hamburger').click(function(){
+		$('.hamburger').toggleClass('active');
+		$('.menu').toggleClass('active');
+	})
+
+});
 
 
 
